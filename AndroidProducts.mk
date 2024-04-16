92117909e1c12d4316cb19d30a9a1c3b728c0867
Ilya <90723091+ilyaa07@users.noreply.github.com>
2024-04-16 19:49:51 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := device/samsung/fortuna3g
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_fortuna3g.mk
+	$(LOCAL_DIR)/dot_fortuna3g.mk
 
 COMMON_LUNCH_CHOICES := \
-	lineage_fortuna3g-eng \
-	lineage_fortuna3g-userdebug \
-	lineage_fortuna3g-user
+	dot_fortuna3g-eng \
+	dot_fortuna3g-userdebug \
+	dot_fortuna3g-user
 
